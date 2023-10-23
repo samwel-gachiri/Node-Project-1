47d7590a459564cf241450b07f9076ab4b780409
samwelG <samgachiri2002@gmail.com>
2023-10-23 16:54:29 +0300

    new commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    res.writeHead('content-type', 'text/plain')
+    res.write('Hello world')
+})
+
+server.listen(proces.env.PORT || 3000, ()=>{
+    console.log('Server is listening on port 3000');
+})
